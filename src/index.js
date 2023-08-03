086dc797ec79e442e06332d105274c70b947c0da
toryzb <tory.zb@gmail.com>
2023-08-03 17:16:42 +0300

    feat: the solution of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+    let str = '';
+    n = String(n);
+    if (n[0] === '-') {
+        n = n.slice(1);
+    }
+    for (let i = 0; i < n.length; i++) {
+      str = n[i] + str;
+    }
+    let i = 0;
+    while (str[i] === 0) {
+        str = str.slice(1);
+        i += 1;
+    }
+    return Number(str);
 }
